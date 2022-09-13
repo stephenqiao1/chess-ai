501ff4d09dd7e83e7706c1f874c62fc84fde5e9d
Stephen Qiao <stephenqiao123@gmail.com>
2022-09-12 19:06:27 -0700

    Created simple move pieces feature
    - also added simple chess notation

--- Chess/ChessEngine.py
+++ Chess/ChessEngine.py
@@ -17,3 +17,29 @@ class ChessState():
             ]
         self.whiteTurn = True
         self.moveLog = []
+    def makeMove(self, move):
+        self.board[move.startRow][move.startCol] = '/'
+        self.board[move.endRow][move.endCol] = move.pieceMoved
+        self.moveLog.append(move)
+        self.whiteTurn = not self.whiteTurn
+
+class Move():
+    ranksToRows = {"1" : 7, "2" : 6, "3" : 5, "4" : 4, "5" : 3, "6" : 2, "7" : 1, "8" : 0}
+    rowsToRanks = {v: k for k, v in ranksToRows.items()}
+    filesToCols = {"a" : 0, "b" : 1, "c" : 2, "d" : 3, "e" : 4, "f" : 5, "g" : 6, "h" : 7}
+    colsToFiles = {v: k for k, v in filesToCols.items()}
+    def __init__(self, startSqr, endSqr, board):
+        self.startRow = startSqr[0]
+        self.startCol = startSqr[1]
+        self.endRow = endSqr[0]
+        self.endCol = endSqr[1]
+        self.pieceMoved = board[self.startRow][self.startCol]
+        self.pieceCaptured = board[self.endRow][self.endCol]
+
+
+    def get_chess_notation(self):
+        return self.get_rank_file(self.startRow, self.startCol) + self.get_rank_file(self.endRow, self.endCol)
+
+
+    def get_rank_file(self, row, col):
+        return self.colsToFiles[col] + self.rowsToRanks[row]
--- Chess/ChessMain.py
+++ Chess/ChessMain.py
@@ -10,41 +10,58 @@ DIMENSION = 8
 SQ_SIZE = HEIGHT // DIMENSION
 MAX_FPS = 30
 IMAGES = {}
+currMoves = ()
 
 def main():
     p.init()
     screen = p.display.set_mode((WIDTH, HEIGHT))
     clock = p.time.Clock()
     gs = ChessEngine.ChessState()
+    sqrSelected = ()  # keep track of the last click of the user (tuple: (rank, file))
+    playerClicks = [] # keep track of player clicks (two tuples: [(6, 4), (4, 4)])
     pieces = ['wP', 'wR', 'wN', 'wK', 'wB', 'wQ', 'bP', 'bR', 'bN', 'bK', 'bB', 'bQ']
     for piece in pieces:
         IMAGES[piece] = p.transform.scale(p.image.load("images/" + piece + ".png"), (SQ_SIZE, SQ_SIZE))
     running = True
     while running:
-        for e in p.event.get(): #Loops through every event in the queue
+        for e in p.event.get():  # Loops through every event in the queue
             if e.type == p.QUIT:
                 running = False
-            drawBoard(screen, gs.board)
-            clock.tick(MAX_FPS)
-            p.display.flip() #updates the display
+            elif e.type == p.MOUSEBUTTONDOWN:
+                location = p.mouse.get_pos()
+                col = location[0] // SQ_SIZE
+                row = location[1] // SQ_SIZE
+                if sqrSelected == (row, col):
+                    sqrSelected = ()
+                    playerClicks = []
+                else:
+                    sqrSelected = (row, col)
+                    playerClicks.append(sqrSelected)
+                if len(playerClicks) == 2:
+                    move = ChessEngine.Move(playerClicks[0], playerClicks[1], gs.board)
+                    print(move.get_chess_notation())
+                    gs.makeMove(move)
+                    sqrSelected = ()
+                    playerClicks = []
 
-def drawBoard(screen, board):
-    colors = [p.Color("antiquewhite"), p.Color("bisque4")] #antiquewhite will act as the white square, and bisque4 as black
-    for rank in range(DIMENSION):
-        for file in range(DIMENSION):
-            if (rank + file) % 2 == 0:
+            draw_board(screen, gs.board)
+            clock.tick(MAX_FPS)
+            p.display.flip()  # updates the display
+
+def draw_board(screen, board):
+    colors = [p.Color("antiquewhite"), p.Color("bisque4")]  # antiquewhite will act as the white square, and bisque4
+                                                            # as black
+    for row in range(DIMENSION):
+        for col in range(DIMENSION):
+            if (row + col) % 2 == 0:
                 color = colors[0]
             else:
                 color = colors[1]
-            p.draw.rect(screen, color, p.Rect(file*SQ_SIZE, rank*SQ_SIZE, SQ_SIZE, SQ_SIZE))
-            piece = board[rank][file]
+            p.draw.rect(screen, color, p.Rect(col*SQ_SIZE, row*SQ_SIZE, SQ_SIZE, SQ_SIZE))
+            piece = board[row][col]
             if piece != "/":
-                screen.blit(IMAGES[piece], p.Rect(file*SQ_SIZE, rank*SQ_SIZE, SQ_SIZE, SQ_SIZE))
+                screen.blit(IMAGES[piece], p.Rect(col*SQ_SIZE, row*SQ_SIZE, SQ_SIZE, SQ_SIZE))
+
 
 if __name__ == "__main__":
     main()
-
-
-
-
-
